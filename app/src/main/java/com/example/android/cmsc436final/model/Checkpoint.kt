9e2b451a3c323d0c73431f5a813027dbb15e7127
ttran210 <tifft213@gmail.com>
2019-11-20 00:03:04 -0500

    added firestore storage stuff

--- app/src/main/java/com/example/android/cmsc436final/model/Checkpoint.kt
+++ app/src/main/java/com/example/android/cmsc436final/model/Checkpoint.kt
@@ -1,7 +1,11 @@
 package com.example.android.cmsc436final.model
 
+import com.google.firebase.firestore.GeoPoint
+
 data class Checkpoint (
     var name: String = "",
+//    TODO: get current location
+    var location: GeoPoint = GeoPoint(0.0,0.0),
     var media: ArrayList<String> = arrayListOf(),
     var description:  String = ""
 )

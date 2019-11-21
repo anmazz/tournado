e22a97ccf438e531e15bca9a7640e275f997d823
ronzav15 <57227699+ronzav15@users.noreply.github.com>
2019-11-21 00:51:34 -0500

    Authentication is at about 85%. Worked on login and registration files and layouts. Also am about 70% there with integrating firebase for new user storage

--- app/src/main/java/com/example/android/cmsc436final/LoginActivity.kt
+++ app/src/main/java/com/example/android/cmsc436final/LoginActivity.kt
@@ -8,22 +8,38 @@ import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.example.locationbasedtourguide.ui.home.HomeFragment
 import java.util.Random
+import com.google.firebase.auth.FirebaseAuth
+
+
 
 class LoginActivity : Activity() {
 
     private lateinit var uname: EditText
     private lateinit var passwd: EditText
+    private lateinit var auth: FirebaseAuth
+    private lateinit var progressBar: ProgressBar
+    private var TAG = "Something Bad Happened"
+// ...
+
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        auth = FirebaseAuth.getInstance()
         uname = findViewById(R.id.email)
         passwd = findViewById(R.id.password)
+        progressBar = findViewById(R.id.progressBar)
 
+    }
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        //updateUI(currentUser)
     }
 
     fun onClick(v: View?) {
@@ -33,43 +49,43 @@ class LoginActivity : Activity() {
                 this@LoginActivity,
                 RegistrationActivity::class.java
             )
-
             startActivity(helloAndroidIntent)
+            return
+
         } else
             //user is logging in: validate input
-            if (!TextUtils.isEmpty(uname.text) &&
-                !TextUtils.isEmpty(passwd.text)
-                && checkPassword(uname.text, passwd.text)
-            ) {
+            progressBar.visibility = View.VISIBLE
 
-                // Create an explicit Intent for starting the HelloAndroid
-                // Activity
-                val helloAndroidIntent = Intent(
-                    this@LoginActivity,
-                    SearchActivity::class.java
-                    //MainActivity::class.java USE THIS TO SEE THE MAP
-                )
-                Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_LONG)
-                    .show()
-                // Use the Intent to start the HelloAndroid Activity
-                startActivity(helloAndroidIntent)
+        val email: String = uname.text.toString()
+        val password: String = passwd.text.toString()
 
-            } else {
-                Toast.makeText(
-                    applicationContext,
-                    "Login failed! Please try again",
-                    Toast.LENGTH_LONG
-                ).show()
-                uname.text.clear()
-                passwd.text.clear()
+        if (TextUtils.isEmpty(email)) {
+           // Toast.makeText(applicationContext, "Please enter email...", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(applicationContext, "Please enter password!", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        auth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                progressBar.visibility = View.GONE
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_LONG)
+                        .show()
+                    startActivity(Intent(
+                        this@LoginActivity,
+                        SearchActivity::class.java))
+                    //MainActivity::class.java USE THIS TO SEE THE MAP)
+                } else {
+                    Log.w(TAG, "signInUserWithEmail:failure", task.exception)
+                    Toast.makeText(applicationContext, "Login failed! Please try again", Toast.LENGTH_LONG).show()
+                    uname.text.clear()
+                    passwd.text.clear()
+                }
             }
+
     }
 
-
-    private fun checkPassword(uname: Editable, passwd: Editable): Boolean {
-        // Just pretending to extract text and check password
-        Log.i("Hi", uname.toString())
-        Log.i("Hi", passwd.toString())
-        return true
-    }
 }

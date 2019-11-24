ae0289a30f21ef83493fcdaa6ce2021d53b5b329
ronzav15 <57227699+ronzav15@users.noreply.github.com>
2019-11-24 01:25:07 -0500

    login and registration 100%

--- app/src/main/java/com/example/android/cmsc436final/ui/userProfile/UserProfileFragment.kt
+++ app/src/main/java/com/example/android/cmsc436final/ui/userProfile/UserProfileFragment.kt
@@ -9,8 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.cmsc436final.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class UserProfileFragment : Fragment() {
+    //if sign out button pressed - sign out
+    //if change profile pic button pressed - change profile pic
 
     private lateinit var userProfileViewModel: UserProfileViewModel
 
@@ -22,10 +25,14 @@ class UserProfileFragment : Fragment() {
         userProfileViewModel =
             ViewModelProviders.of(this).get(UserProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_user_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.ratingText)
+
+        //val textView: TextView = root.findViewById(R.id.ratingText)
         userProfileViewModel.text.observe(this, Observer {
-            textView.text = it
+            //textView.text = it
         })
         return root
     }
+
+
+
 }

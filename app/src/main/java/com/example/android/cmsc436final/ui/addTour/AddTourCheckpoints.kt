846a9260c8cdcfcbcd6a0ed9c51cb18aedf74a59
Tiffany <46389573+ttran210@users.noreply.github.com>
2019-12-02 17:07:50 -0500

    infotab doesnt crash anymore

--- app/src/main/java/com/example/android/cmsc436final/ui/addTour/AddTourCheckpoints.kt
+++ app/src/main/java/com/example/android/cmsc436final/ui/addTour/AddTourCheckpoints.kt
@@ -1,4 +1,106 @@
 package com.example.android.cmsc436final.ui.addTour
 
-class AddTourCheckpoints {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import com.example.android.cmsc436final.R
+import com.example.android.cmsc436final.SharedViewModel
+import com.example.android.cmsc436final.model.Checkpoint
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.firestore.GeoPoint
+
+class AddTourCheckpoints: Fragment() {
+
+    private lateinit var sharedViewModel: SharedViewModel
+    private lateinit var checkptName: TextInputEditText
+    private lateinit var checkptDesc: TextInputEditText
+//    Buttons
+    private lateinit var buttonAddLocation: Button
+    private lateinit var buttonAddPicture: Button
+    private lateinit var buttonAddVideo: Button
+    private lateinit var buttonAddAudio: Button
+    private lateinit var buttonAddCheckpoint: Button
+    private lateinit var buttonNext: Button
+
+    // ArrayList of Checkpoints
+    private lateinit var checkpoints: MutableList<Checkpoint>
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        sharedViewModel =
+            ViewModelProviders.of(this).get(sharedViewModel::class.java)
+
+        val root = inflater.inflate(R.layout.fragment_add_tour_1, container, false)
+
+
+        // UI elements
+        checkptName = root.findViewById<View>(R.id.cp_name) as TextInputEditText
+        checkptDesc = root.findViewById<View>(R.id.cp_descrip) as TextInputEditText
+
+        // buttons
+        buttonAddLocation = root.findViewById<View>(R.id.add_cp_location_button) as Button
+        buttonAddPicture = root.findViewById<View>(R.id.add_cp_picture_button) as Button
+        buttonAddVideo = root.findViewById<View>(R.id.add_cp_video_button) as Button
+        buttonAddAudio = root.findViewById<View>(R.id.add_cp_audio_button) as Button
+
+        buttonNext = root.findViewById<View>(R.id.next_button) as Button
+
+        buttonAddCheckpoint = root.findViewById<View>(R.id.add_cp_button) as Button
+
+        // list of checkpoints we keep adding to
+        checkpoints = ArrayList()
+
+
+//        TODO add onclicks
+
+        buttonNext.setOnClickListener() {
+            saveAndNext()
+        }
+
+//        TODO make sure this is adding just pictures
+        buttonAddPicture.setOnClickListener() {
+            navigateToAddMedia()
+        }
+
+        return root
+    }
+
+    // TODO make sure this is adding just pictures
+    private fun navigateToAddMedia(){
+        findNavController().navigate(R.id.action_navigation_add_tour_to_navigation_add_media)
+    }
+
+    // TODO make sure this is adding just audio
+    private fun navigateToAddAudio(){
+        findNavController().navigate(R.id.action_navigation_add_tour_to_navigation_add_media)
+    }
+
+    // TODO make sure this is adding just video
+    private fun navigateToAddVideo(){
+        findNavController().navigate(R.id.action_navigation_add_tour_to_navigation_add_media)
+    }
+
+    fun saveAndNext() {
+        // get strings from textboxes
+        val tourNameStr = checkptName.text.toString().trim { it <= ' ' }
+        val tourDescripStr = checkptDesc.text.toString()
+
+        // add to viewModel
+        sharedViewModel.addCheckpoints(checkpoints)
+    }
+
+    //    TODO navigate to the add tags page
+    private fun navigateToAddTags(){
+        findNavController().navigate(R.id.action_navigation_add_tour_to_navigation_add_media)
+    }
+
 }
--- app/src/main/java/com/example/android/cmsc436final/ui/tourOverview/InfoTab.kt
+++ app/src/main/java/com/example/android/cmsc436final/ui/tourOverview/InfoTab.kt
@@ -47,7 +47,7 @@ import org.json.JSONObject
  *
  */
 
-class InfoTab: Fragment(), CheckpointAdapter.OnCheckpointSelectedListener {
+class InfoTab: Fragment(){
     private lateinit var mModel: SharedViewModel
 
     private var currTour: Tour? = null
@@ -66,11 +66,6 @@ class InfoTab: Fragment(), CheckpointAdapter.OnCheckpointSelectedListener {
         fun newInstance(): InfoTab = InfoTab()
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        mModel = ViewModelProviders.of(this).get(SharedViewModel::class.java)
-    }
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -82,82 +77,8 @@ class InfoTab: Fragment(), CheckpointAdapter.OnCheckpointSelectedListener {
 //        var tourid = arguments?.getString("tourid")
 //        getTourById(tourid!!)
         val root = inflater.inflate(R.layout.tour_overview_info_tab, container, false)
-        mCheckpointsRecycler = root.findViewById(R.id.recycler_checkpoints)
-
-        // Initialize Firestore and the main RecyclerView
-        initFirestore()
-        initRecyclerView()
 
 
         return root
     }
-
-    override fun onStart() {
-        super.onStart()
-        // Start listening for Firestore updates
-        if (mAdapter != null) {
-            mAdapter!!.startListening()
-        }
-    }
-
-    override fun onStop(){
-        super.onStop()
-        if (mAdapter != null) {
-            mAdapter!!.startListening()
-        }
-    }
-
-    private fun initFirestore() {
-        mQuery = mFirestore!!.collection("tours").limit(InfoTab.LIMIT.toLong())
-    }
-
-    private fun initRecyclerView() {
-        if (mQuery == null) {
-            Log.w(InfoTab.TAG, "No query, not initializing RecyclerView")
-        }
-
-        mAdapter = object: CheckpointAdapter(mQuery, this) {
-            override fun onDataChanged() { // Show/hide content if the query returns empty.
-                if (itemCount == 0) {
-                    mCheckpointsRecycler!!.visibility = View.GONE
-                } else {
-                    mCheckpointsRecycler!!.visibility = View.VISIBLE
-                }
-            }
-
-            override fun onError(e: FirebaseFirestoreException?) { // Show a snackbar on errors
-                Snackbar.make(
-                    activity!!.findViewById<View>(android.R.id.content),
-                    "Error: check logs for info.", Snackbar.LENGTH_LONG
-                ).show()
-            }
-        }
-        mCheckpointsRecycler!!.layoutManager = LinearLayoutManager(context)
-        mCheckpointsRecycler!!.adapter = mAdapter
-    }
-
-    private fun getTourById(tourId: String) {
-        db = FirebaseFirestore.getInstance()
-        val docRef = db.collection("tours").document(tourId)
-        docRef.get().addOnSuccessListener { document ->
-            if (document != null) {
-                Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                currTour = document.toObject(Tour::class.java)
-            } else {
-                Log.d(TAG, "No such document")
-            }
-        }
-            .addOnFailureListener { exception ->
-                Log.d(TAG, "get failed with ", exception)
-            }
-    }
-
-    override fun onCheckpointSelected (checkpoint: DocumentSnapshot?) {
-        lifecycleScope.launch {
-            mModel.selectTour(tour!!.id)
-        }
-        val bundle = bundleOf("tourid" to tour!!.id)
-        findNavController().navigate(R.id.action_navigation_home_to_tour_overview, bundle)
-    }
-
 }

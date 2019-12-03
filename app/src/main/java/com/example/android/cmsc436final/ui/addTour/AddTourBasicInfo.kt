c14768cb97ac8a636c5fbeff676bf50f1f096186
ronzav15 <57227699+ronzav15@users.noreply.github.com>
2019-12-03 00:05:46 -0500

    Update AddTourBasicInfo.kt

--- app/src/main/java/com/example/android/cmsc436final/ui/addTour/AddTourBasicInfo.kt
+++ app/src/main/java/com/example/android/cmsc436final/ui/addTour/AddTourBasicInfo.kt
@@ -37,6 +37,7 @@ class AddTourBasicInfo: Fragment() {
         tourDescrip = root.findViewById<View>(R.id.tour_descrip) as TextInputEditText
         buttonAddPicture = root.findViewById<View>(R.id.add_tour_picture_button) as Button
         buttonNext = root.findViewById<View>(R.id.next_button) as Button
+        buttonCancel = root.findViewById<View>(R.id.cancel_button) as Button
 
 
         buttonNext.setOnClickListener() {

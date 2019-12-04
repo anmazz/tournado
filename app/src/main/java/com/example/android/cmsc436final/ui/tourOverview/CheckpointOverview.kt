d662f4e7eb5563c30952feb47ddbe367a278b6fa
Anna Mazzanti <annamazzanti@gmail.com>
2019-12-03 19:52:56 -0500

    adding check when user is done

--- app/src/main/java/com/example/android/cmsc436final/ui/tourOverview/CheckpointOverview.kt
+++ app/src/main/java/com/example/android/cmsc436final/ui/tourOverview/CheckpointOverview.kt
@@ -1,22 +1,27 @@
 package com.example.android.cmsc436final.ui.tourOverview
 
+import android.app.Dialog
 import android.location.Geocoder
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.android.cmsc436final.R
 import com.example.android.cmsc436final.SharedViewModel
 import com.example.android.cmsc436final.model.Checkpoint
+import com.example.android.cmsc436final.ui.startTour.StartTourFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
@@ -54,8 +59,10 @@ class CheckpointOverview: Fragment() {
         audioButton = root.findViewById(R.id.audio_button)
         videoButton = root.findViewById(R.id.video_button)
         continueButton = root.findViewById(R.id.continue_button)
+
+
         mModel = ViewModelProviders.of(activity!!).get(SharedViewModel::class.java)
-        var tour = mModel.getCurrentTour()
+        var tour = mModel.getCurrentTour()!!
         currCheckpoint = tour!!.checkpoints?.get(mModel.getCurrentCheckpointNum())!!
 
         nameView.text = (currCheckpoint!!.name)
@@ -67,9 +74,17 @@ class CheckpointOverview: Fragment() {
 //        val lat = currCheckpoint.location.latitude
 //        val long = currCheckpoint.location.longitude
 //        locationView.text = geocoder.getFromLocation(lat, long, 1)[0].locality
+        continueButton.setOnClickListener{
+            if(mModel.getCurrChkptNumStartTour()==tour.checkpoints!!.size-1){
+                val title = "Congrats!"
+                val body = "You just completed the ${tour.name} tour!"
+                showDialog(title, body)
+            } else {
+                findNavController().navigate(R.id.action_checkpoint_overview_to_start_tour)
+            }
+        }
 
-
-        audioButton.setOnClickListener() {
+        audioButton.setOnClickListener {
             if (currCheckpoint.audio.equals("")) {
                 Toast.makeText(context, "No audio available", Toast.LENGTH_LONG).show()
             } else {
@@ -86,6 +101,28 @@ class CheckpointOverview: Fragment() {
         }
 
         return root
+    }
+
+    private fun showDialog(title: String, body: String) {
+        val dialog = Dialog(activity!!)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.dialog_checkpoint)
+
+        val titleView = dialog.findViewById(R.id.tvTitle) as TextView
+        val bodyView = dialog.findViewById(R.id.tvBody) as TextView
+        titleView.text = title
+        bodyView.text = body
+        val yesBtn = dialog .findViewById(R.id.btn_yes) as Button
+        yesBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_checkpoint_overview_to_navigation_home)
+            //TODO: increase user tours done count
+
+            mModel.reset()
+            dialog.dismiss()
+        }
+        dialog.show()
+
     }
 
     fun playAudio() {
